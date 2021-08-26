b1eb647471ed89f277b456ed17fb8ce525aa1ae3
Charles-Homks <charles01homks@gmail.com>
2021-08-26 02:37:40 -0700

    Using the puts C command

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
-init main(void)
+ * main - Entry point
+ *
+ * puts - To write a new line or string to the output. It prints
+ * the passed string with a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-    puts("\"Programming is like building a mutilingual puzzle");
-    return (0);
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
 }
 
