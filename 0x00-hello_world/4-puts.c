b52c78723dc7b7e54f2331f5e6d0f4aa5ca51b5f
Charles-Homks <charles01homks@gmail.com>
2021-08-26 02:35:02 -0700

    Using the puts C command

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+init main(void)
+{
+    puts("\"Programming is like building a mutilingual puzzle");
+    return (0);
+}
 
