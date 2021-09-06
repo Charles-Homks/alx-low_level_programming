6ee3861010c0a40a428523761ed0633a170b636b
Charles-Homks <charles01homks@gmail.com>
2021-09-06 11:13:18 -0700

    Game of alphabet x 10.0

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,7 @@
 #include "main.h"
 
 /**
- * main - check the code.
- *
- * Return: Always 0.
- *
- * print_alphabet_x10 - print lower case alphabet 10 times.
+ * print_alphabet_x10 - prints lower case alphabet 10 times.
  */
 void print_alphabet_x10(void)
 {
