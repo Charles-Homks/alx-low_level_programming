7610c4dd2fd28e45dc334503a81bc4f462aa7978
Charles-Homks <charles01homks@gmail.com>
2021-10-19 06:42:26 -0700

    Using the puts and printf format functions

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * puts: use to write a line or string to the out stdout
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
