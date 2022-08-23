8ba2bdbf1e831b5656ab6e5b90fda25255858c8b
Charles Homsuk <charles01homks@gmail.com>
2022-08-22 17:32:07 -0700

    C program that prints exactly "Programming is like building a multilingual puzzle,

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char Dora[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(Dora);
+	return (0);
+}
