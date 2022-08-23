65884a467ff988dfead6d9b900b167412d22fd8c
Charles Homsuk <charles01homks@gmail.com>
2022-08-23 07:20:37 -0700

    C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19,

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char Dora[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	size_t size = sizeof(Dora);
+	write(1, Dora, size);
+	return (1);
+}
