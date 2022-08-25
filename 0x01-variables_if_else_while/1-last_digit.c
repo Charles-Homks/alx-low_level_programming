ccffcbd33bb0951d944baa5daba7afe27f70edcb
Charles Homsuk <charles01homks@gmail.com>
2022-08-24 19:43:45 -0700

    program prints last digit of a number and makes decision

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,14 +1,31 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
 
-/* betty style doc for function main goes there */
+/**
+ * main - Function assign random variable to n each time executed and prints
+ * the last digit of the number stored in the variable n
+ *
+ * Return: Always 0 (success)
+*/
 int main(void)
 {
 	int n;
+	int last;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+	last = n % 10;
+	printf("Last digit of %d is %d ", n, last);
+	if (last > 5)
+	{
+		printf("and is greater than 5\n");
+	} else if (last == 0)
+	{
+		printf("and is 0\n");
+	} else if (last < 6 && last != 0)
+	{
+		printf("and is less than 6 and not 0\n");
+	}
 	return (0);
 }
