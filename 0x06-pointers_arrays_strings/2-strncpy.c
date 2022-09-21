b832af95a8cac8e937e86731dde7f59d4d19f72f
Charles Homsuk <charles01homks@gmail.com>
2022-09-20 23:45:39 -0700

    function that copies a string of n bytes plus (*)

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncpy - function copies the string pointed to by src
+ * to the buffer pointed to by dest.
+ *
+ * @src: Pointer to string source.
+ * @dest: Pointer to string destination.
+ * @n: number of bytes to copy.
+ * Return: pointer to dest.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (src[i] && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
