432c9983497d90e3bec8e7e266a03b074d45223d
Charles Homsuk <charles01homks@gmail.com>
2022-09-22 13:52:40 -0700

    A function that prints an integer(to be edited).

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_number - function prints an integer values.
+ * @n: integer variable to print
+ * Return: void
+ */
+void print_number(int n)
+{
+	_putchar(n + '0');
+}
