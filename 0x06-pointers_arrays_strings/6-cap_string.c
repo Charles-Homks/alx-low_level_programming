26519ef8cb3f84f328f6dd61e4b4e6a6f6134b19
Charles Homsuk <charles01homks@gmail.com>
2022-09-22 03:23:11 -0700

    A function that capitalizes all words of a string.

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -35,14 +35,7 @@ char *cap_string(char *str)
 			if (str[i] >= 'a' && str[i] <= 'z')
 				str[i] -= 32;
 		}
-	}	
-/**
- * else
- * {
- * if (str[i] >= 'A' && str[i] <= 'Z')
- * str[i] += 32;
- * }
- */
+	}
 
 	return (str);
 }
